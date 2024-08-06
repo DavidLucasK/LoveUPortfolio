97bdee171a2cb6d373cc966e13b5436cc66032fa
David Lucas <david@gorilla.com.br>
2024-08-06 20:24:48 -0300

    Changing endpoint to profile points

--- scripts/profile.js
+++ scripts/profile.js
@@ -66,7 +66,7 @@ function uploadImage(file) {
 
 async function fetchUserPoints() {
     try {
-        const response = await fetch(`${backendUrl}/points-teste`, {
+        const response = await fetch(`${backendUrl}/points-test`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
